5498403845584606087987ddeb3f455cbd8769ad
Eimer <steamdown1@gmail.com>
2026-01-15 08:37:54 +0200

    added reviews section and some minor style fixes

--- src/app/features/reviews/reviews.ts
+++ src/app/features/reviews/reviews.ts
@@ -1,11 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+import { Stars } from '../../shared/stars/stars';
+import { CustomSlider } from '../../shared/custom-slider/custom-slider';
 
 @Component({
   selector: 'app-reviews',
-  imports: [],
+  imports: [Stars, CustomSlider],
   templateUrl: './reviews.html',
   styleUrl: './reviews.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Reviews {
   @Input() data: any;
+
+  readonly sliderConfig = {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    navigation: true,
+    centeredSlides: true,
+    autoplay: { delay: 2500 },
+  };
+
+  readonly sliderMobileConfig = {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    loop: true,
+    navigation: true,
+    centeredSlides: true,
+    autoplay: { delay: 2500 },
+  };
+
+
 }
