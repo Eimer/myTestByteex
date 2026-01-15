2a92440a79f3ca707a2d99c38568c3cc2caff981
Eimer <steamdown1@gmail.com>
2026-01-15 10:02:16 +0200

    added-faq-section

--- src/app/features/faq/faq.ts
+++ src/app/features/faq/faq.ts
@@ -1,11 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { Stars } from '../../shared/stars/stars';
 
 @Component({
   selector: 'app-faq',
-  imports: [],
+  imports: [Stars],
   templateUrl: './faq.html',
   styleUrl: './faq.scss',
 })
 export class Faq {
   @Input() data: any;
+
+  activeIndex: number | null = null;
+
+  toggleAccordion(index: number): void {
+
+    this.activeIndex = this.activeIndex === index ? null : index;
+  }
+
 }
